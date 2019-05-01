f5375d1545499253f196a089187cf0d000d04501
shliahtur <shliahtur@gmail.com>
2019-05-01 16:32:59 +0300

    vadim

--- EstellApi/ClientApp/src/components/NavMenu.js
+++ EstellApi/ClientApp/src/components/NavMenu.js
@@ -10,7 +10,7 @@ export default props => (
           <Link className="nav-logo" to={'/'}></Link>
           <Link to={'/'} exact>Бренды</Link>
           <Link to={'/counter'}>Новости</Link>
-          <Link to={'/fetchdata'}>ggg</Link>
+          <Link to={'/fetchdata'}>11111111111</Link>
           <Link to={'/cart'}>vadim лох</Link>
     </nav>
   </div>
