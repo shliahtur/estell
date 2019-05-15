df00f6020030e32cdfd868bca156a52afa603f04
shliahtur <shliahtur@gmail.com>
2019-05-15 22:18:33 +0300

    pics added

--- EstellApi/ClientApp/src/components/ProductList.js
+++ EstellApi/ClientApp/src/components/ProductList.js
@@ -2,23 +2,26 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Preloader from './Preloader';
+import {products} from '../data/tempProducts';
 
 class ProductList extends Component {
 
   render() {
-    if(this.props.products.length) {
+    if(products.length) {
       return (
         <div>
         
-          {this.props.products.map(product => {
+          {products.map(product => {
             return (
               <div key={ product.id }>
-                <hr/>          
+                <hr/>    
+                  
+                <img src={product.imagePath} height={"200px"} alt={product.name} />        
                 <h4><Link to={`/products/${product.id}`}>{product.id}: {product.name}</Link></h4>
                 <p>{product.price}</p>
                 <p>{product.vendorCode}</p>
                 <p>{product.age}</p>
-                <p>{product.imagePath}</p>
+               
               </div>
             );
           })}
@@ -30,6 +33,8 @@ class ProductList extends Component {
   }
 }
 
-const mapStateToProps = (state) => ({ products: state.products });
+// const mapStateToProps = (state) => ({ products: state.products });
 
-export default connect(mapStateToProps)(ProductList);
+// export default connect(mapStateToProps)(ProductList);
+
+export default ProductList;
