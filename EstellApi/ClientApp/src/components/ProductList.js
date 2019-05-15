247a5ff51595edbe48a685f4ad4bb7dd9140335a
Rekunova <ma.rekunova@gmail.com>
2019-05-15 23:55:26 +0300

    cards

--- EstellApi/ClientApp/src/components/ProductList.js
+++ EstellApi/ClientApp/src/components/ProductList.js
@@ -16,11 +16,11 @@ class ProductList extends Component {
             return (
               <div className="product-item" key={ product.id }>   
                 <img className="product-item_img" src={product.imagePath} height={"200px"} alt={product.name} />        
-                <h4>< Link className="produc-item_name" to={`/products/${product.id}`}>{product.name}</Link></h4>
+                <h4>< Link className="product-item_name" to={`/products/${product.id}`}>{product.name}</Link></h4>
                 <p className="product-item_description">what a great toy</p>
                 <p className="product-item_price">{product.price}</p>
                 <p className="product-item_article">{product.vendorCode}</p>
-                <span className="product-item_age">{product.age}</span>
+                <span className="product-item_age product-item_age__yellow">{product.age}</span>
               </div>
               
             );
